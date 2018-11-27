77ecad9da28bf8b7f1c7804f3fbf7d656afffa7b
youssef12001 <45400001+youssef12001@users.noreply.github.com>
2018-11-28 00:39:56 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,82 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = 'البرفيكس حق البوت'
+ 
+ 
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`LastCodes4Ever  `,"https://www.twitch.tv/dggamingbot")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+ 
+ 
+ 
+client.login(process.env.BOT_TOKEN);
+
+
+{
+  "dependencies": {
+    "and": "0.0.3",
+    "anti-spam": "^0.2.7",
+    "array-sort": "^1.0.0",
+    "ascii-data-table": "^2.1.1",
+    "canvas": "^1.6.11",
+    "canvas-prebuilt": "^1.6.5-prerelease.1",
+    "child_process": "^1.0.2",
+    "common-tags": "^1.8.0",
+    "dateformat": "^3.0.3",
+    "discord-anti-spam": "^1.1.2",
+    "discord.js": "^11.3.2",
+    "ffmpeg-binaries": "^4.0.0",
+    "figlet": "^1.2.0",
+    "file-system": "^2.2.2",
+    "forever": "^0.15.3",
+    "fortnite": "^4.1.1",
+    "fortnite-api": "^2.12.2",
+    "get-youtube-id": "^1.0.0",
+    "giphy-api": "^1.2.7",
+    "goo.gl": "^0.1.4",
+    "google-it": "^1.1.3",
+    "google-translate-api": "^2.3.0",
+    "hastebin-gen": "^1.3.1",
+    "i": "^0.3.6",
+    "jimp": "^0.2.28",
+    "math-expression-evaluator": "^1.2.17",
+    "moment": "^2.22.2",
+    "node-opus": "^0.3.0",
+    "npm": "^6.2.0",
+    "opusscript": "0.0.6",
+    "pretty-ms": "^3.2.0",
+    "queue": "^4.4.2",
+    "quick.db": "^6.3.2",
+    "short-number": "^1.0.6",
+    "simple-youtube-api": "^5.0.2",
+    "sqlite": "^2.9.2",
+    "table": "^4.0.3",
+    "winston": "^3.0.0",
+    "youtube-info": "^1.2.0",
+    "ytdl-core": "^0.21.1",
+    "zalgolize": "^1.2.4"
+  }
+}
